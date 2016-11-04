0c368af03112be6acc595f88632e94cc75fbf34b
gamerrishad <ehsanulbigboss@gmail.com>
2016-11-04 20:40:57 +0600

    Update Mathematics.cpp

--- Mathematics.cpp
+++ Mathematics.cpp
@@ -173,3 +173,25 @@ for (int mask = 0 ; mask < lmt ; mask++)
 }
 return ans;
 // *** END OF Number of Solutions to a Linear Algebraic Equation with Upper and Lower Bound ***************
+
+// **** Euler Phi Function ****************** //
+const int MAX = 1000001;
+int phi[MAX];
+
+void euler_phi()
+{
+    phi[1] = 1;
+    for(int i=2; i<MAX; i++)
+    {
+        if(!phi[i])
+        {
+            phi[i] = i-1;
+            for(int j=(i<<1); j<MAX; j+=i)
+            {
+                if(!phi[j]) phi[j] = j;
+                phi[j] = phi[j]/i*(i-1);
+            }
+        }
+    }
+}
+// ****** END of Euler Phi Function // ************
